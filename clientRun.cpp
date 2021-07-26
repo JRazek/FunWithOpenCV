c4c400ff0ccea6ddc1b4ff1fc46b8ecd3f7bb7ff
JRazek <jakub.jakubecky123@gmail.com>
2021-07-26 23:59:55 +0200

    2 clients

--- clientRun.cpp
+++ clientRun.cpp
@@ -11,8 +11,11 @@
 int main() {
     const char *ip = "10.0.0.9";
     const u_short port = 12321;
-    TCPClient tcpClient(ip, port);
-    if(tcpClient.connect() < 0)
+    TCPClient tcpClient1(ip, port);
+    if(tcpClient1.connect() < 0)
+        return -1;
+    TCPClient tcpClient2(ip, port);
+    if(tcpClient2.connect() < 0)
         return -1;
     cv::VideoCapture cam(0);
     cv::Mat video;
@@ -20,15 +23,17 @@ int main() {
     while(1) {
         const int packetCount = 1000;
 
-        tcpClient.sendPacketsMetaData(packetCount);
+        tcpClient1.sendPacketsMetaData(packetCount);
+        tcpClient2.sendPacketsMetaData(packetCount);
         for (int i = 0; i < packetCount; i++) {
             cam.read(video);
             byte *data = ByteImage::encodeImage(video);
-            if(tcpClient.sendPacket(data, ByteImage::getDataSize(data) + ByteImage::imageMetadataSize) < 0)
+            if(tcpClient1.sendPacket(data, ByteImage::getDataSize(data) + ByteImage::imageMetadataSize) < 0)
+                return -1;
+            if(tcpClient2.sendPacket(data, ByteImage::getDataSize(data) + ByteImage::imageMetadataSize) < 0)
                 return -1;
             cv::waitKey(16);
             delete data;
         }
     }
-    return 0;
 }
